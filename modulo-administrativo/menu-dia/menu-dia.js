c554acf584d9a2600afaa238643fe32f140092bc
Dehymoss <holguinvcruz@hotmail.com>
2025-06-08 23:08:14 -0500

    Actualización de Éter: refinamiento de presentación y corrección del nombre del cliente

--- modulo-administrativo/menu-dia/menu-dia.js
+++ modulo-administrativo/menu-dia/menu-dia.js
@@ -1,11 +1,19 @@
 document.getElementById('form-menu-dia').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const formData = new FormData(this);
+    const data = {
+        sopa: this.sopa.value,
+        plato: this.plato.value,
+        bebida: this.bebida.value,
+        postre: this.postre.value
+    };
 
-    fetch('guardar-menu.php', {
+    fetch('https://script.google.com/macros/s/AKfycbxfrS8_exUHSrmYNFxhFkk4u6FzHtejY1m5UA5Ba3Xe4Bs2iEjKqw0yYaS8VJvzqKA/exec', {
         method: 'POST',
-        body: formData
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
     .then(response => response.ok ? response.text() : Promise.reject('Error en la respuesta'))
     .then(data => {
